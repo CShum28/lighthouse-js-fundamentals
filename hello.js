66d036ff69297e40347f8eeb18c9fda387f673b0
Clement Shum <clement.shum128@gmail.com>
2022-11-17 20:51:32 -0500

    Function with return statement and logging the name

--- hello.js
+++ hello.js
@@ -1,7 +1,6 @@
-const sayHello = function (name){
-	console.log("Hello, " + name);
+const sayHelloToConsole = function (name){
+	return "Hello, "+name;
 }
 
-sayHello("Caliban");
-sayHello("Miranda");
-sayHello("Ferdinand");
+const greeting = sayHelloToConsole('John');
+console.log(greeting);
